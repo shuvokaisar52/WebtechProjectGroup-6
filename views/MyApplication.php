f8fabd1d171b007e3dcff92485cd91aa016e0c3e
shuvokaisar52 <kaisarshuvo52@gmail.com>
2026-05-17 21:51:32 +0600

    Update appilcation page v2

--- views/MyApplication.php
+++ views/MyApplication.php
@@ -3,61 +3,17 @@ session_start();
 include "../models/db.php";
 
 $user_id = $_SESSION["user_id"];
-$database = new db();
-$connection = $database->connection();
-
-$result = $database->getMyApplications($connection, $user_id);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>My Applications Page</title>
 </head>
-<body>
+<script src="../controllers/js/FetchApplications.js"></script>
+<body onload="FetchApplications()">
 <h2 style="text-align:center;">My Applications</h2>
 
-<?php
-if($result && $result->num_rows > 0){
-	while($row = $result->fetch_assoc()){
-
-		$job_id = $row["job_id"];
-
-		$jobResult = $database->getJobById($connection, $user_id);
-		$job = $jobResult->fetch_assoc();
-
-		$emp_id = $job["employer_id"];
-		$empQuery = "SELECT * FROM employer_profiles WHERE user_id='".$emp_id."'";
-		$empResult = $connection->query($empQuery);
-		$company = $empResult->fetch_assoc();
-		if(!$company){
-			$company_name = "No Company";
-		} else {
-			$company_name = $company["company_name"];
-		}
-	?>
-
-	<div style="border:1px solid black; margin:10px; padding:10px;">
-
-		<h3><?php echo $job["title"]; ?></h3>
-		<p>Company Name: <?php echo $company_name; ?></p>
-		<p>Date Applied: <?php echo $row["created_at"]; ?></p>
-		<p>
-			Status: 
-			<span style="padding:5px; border-radius:5px; background-color:lightgray;">
-				<?php echo $row["status"]; ?>
-			</span>
-		</p>
-
-	</div>
-
-<?php 
-	} 
-}else{
-?>
-    <h2 style="text-align:center; color:red;">No application found.</h2>
-<?php
-}
-?>
+<div id="application_list" style="margin-top: 20px;"></div>
 
 </body>
 </html>
